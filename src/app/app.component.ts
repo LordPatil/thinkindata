a256e91ae1462373a0ec4996766abccef7b33820
Lord Patil <chiragnpatil@gmail.com>
2025-02-22 18:59:09 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ScheduleCallComponent } from './schedule-call/schedule-call.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScheduleCallComponent,TestimonialsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/schedule-call/schedule-call.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-call',
+  imports: [],
+  templateUrl: './schedule-call.component.html',
+  styleUrl: './schedule-call.component.css'
+})
+export class ScheduleCallComponent {
+
+}
